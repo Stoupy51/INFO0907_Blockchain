a5eabaefc3c06edd9c89d81bbbea8dda351acb84
Stoupy51 <stoupy51@gmail.com>
2024-11-27 16:50:50 +0100

    perf: ⚡️ hash_sophistique() now pass all tests

--- src/hash.py
+++ src/hash.py
@@ -45,16 +45,17 @@ def hash_sophistique(chaine: str|bytes, nb_octets: int = 8) -> int:
     1. Calcul d'un hash simple (somme des octets) qui servira d'initialisation
     2. Utilisation des décimales de PI et PHI comme source "d'aléatoire" :
        - Sélection d'un index de départ dans PI basé sur le hash simple
-       - Sélection d'un index de départ dans PHI basé sur le carré du hash simple (car faut bien quelque chose de différent)
+       - Sélection d'un index de départ dans PHI basé sur le cube du hash simple (car faut bien quelque chose de différent)
     3. Pour chaque octet de la chaine d'entrée :
        - Utilisation des décimales courantes de PI et PHI pour des opérations arithmétiques
        - Multiplication et division par ces décimales
        - Addition de l'octet multiplié par PI et divisé par PHI
-       - Multiplication par un nombre premier (131 car j'ai envie)
+       - Multiplication par un nombre premier (131 car j'ai envie) que je divise par 2
        - Mise à jour des index de PI et PHI de manière bizarre (7 et 31 sont de beaux nombres premiers non ?)
     4. Application d'un modulo final pour obtenir un hash de la taille souhaitée
 
     Comment m'est venu l'idée ? Et bien au pif, "pourquoi pas utiliser les décimales de PI et PHI ?" et puis j'ai essayé.
+    J'ai ensuite testé de changer quelques petits trucs à droites à gauche jusqu'à tous les tests passent.
 
     >>> hash_sophistique("Hello, world!")
     9718304950604283904
@@ -73,8 +74,8 @@ def hash_sophistique(chaine: str|bytes, nb_octets: int = 8) -> int:
     # Les 100 premières décimales de PI (voir constantes.py), on prend un index de départ en fonction du hash simple
     index_de_pi: int = hash_simple % len(DECIMALES_PI)
 
-    # Les 100 premières décimales du nombre d'or (voir constantes.py), on prend un index de départ en fonction du hash simple (au carré)
-    index_de_phi: int = (hash_simple**2) % len(DECIMALES_PHI)
+    # Les 100 premières décimales du nombre d'or (voir constantes.py), on prend un index de départ en fonction du hash simple (au cube)
+    index_de_phi: int = (hash_simple**3) % len(DECIMALES_PHI)
 
     # à ce stade, on a deux index qui déterminent quelle décimale de PI et de PHI on va utiliser
     # On hache la chaine
@@ -91,8 +92,8 @@ def hash_sophistique(chaine: str|bytes, nb_octets: int = 8) -> int:
         # On ajoute octet multiplié par PHI, on ajoute octet divisé par PHI
         sortie += octet * PI + octet / PHI
 
-        # On multiple par 131 et on prend le modulo
-        sortie = int(sortie * 131) % MODULO
+        # On multiple par 131, puis on décale de 1 bit vers la droite (division par 2) et on prend le modulo
+        sortie = (int(sortie * 131) >> 1) % MODULO
 
         # On update les index (en ajoutant la valeur de la décimale utilisée de l'autre côté + l'octet)
         index_de_pi = 7*(index_de_pi + decimale_de_phi + octet) % len(DECIMALES_PI)
